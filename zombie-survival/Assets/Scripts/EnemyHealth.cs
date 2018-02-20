8baee8f26c107e4a7d065eaa20bbe06422527d51
Dennis Quach <dwquach@gmail.com>
2018-02-19 19:27:26 -0800

    Assets, player, and zombie
    Created a walkable plane and created borders to prevent falling.
    Populated scene with a player and zombie. Added player movement and
    zombie nav agent. Player can shoot but its broken. Light works but line
    renderer buggy. and no hit detection.

---
+++ zombie-survival/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+    public int currentHealth;                   // The current health the enemy has.
+    public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
+    public AudioClip deathClip;                 // The sound to play when the enemy dies.
+
+
+    Animator anim;                              // Reference to the animator.
+    AudioSource enemyAudio;                     // Reference to the audio source.
+    CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
+    bool isDead;                                // Whether the enemy is dead.
+    bool isSinking;                             // Whether the enemy has started sinking through the floor.
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+        if (isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        // If the enemy is dead...
+        if (isDead)
+            // ... no need to take damage so exit the function.
+            return;
+
+        // Play the hurt sound effect.
+        enemyAudio.Play();
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+
+        // If the current health is less than or equal to zero...
+        if (currentHealth <= 0)
+        {
+            // ... the enemy is dead.
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        // The enemy is dead.
+        isDead = true;
+
+        // Turn the collider into a trigger so shots can pass through it.
+        capsuleCollider.isTrigger = true;
+
+        // Tell the animator that the enemy is dead.
+        anim.SetTrigger("Dead");
+
+        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play();
+    }
+
+
+    public void StartSinking()
+    {
+        // Find and disable the Nav Mesh Agent.
+        GetComponent<NavMeshAgent>().enabled = false;
+
+        // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        // The enemy should no sink.
+        isSinking = true;
+
+        // After 2 seconds destory the enemy.
+        Destroy(gameObject, 2f);
+    }
+}
